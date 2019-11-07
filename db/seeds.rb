ee50ff15367876516649584ceedaca36e9892dc7
Rami Burpee <rami.burpee@lootcrate.com>
2019-11-07 09:34:21 -0800

    seed db with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+customers = 2.times.map do
+  Customer.create!(
+    full_name: Faker::Name.name
+  )
+end
+
+10.times.map do
+  client = Client.create!(
+    email: Faker::Internet.email
+  )
+
+  # past
+  start_at = DateTime.now - (rand(1..50)).days
+  end_at = start_at + 1.hour
+  Appointment.create!(
+    start_at: start_at,
+    end_at: end_at,
+    client_id: client.id,
+    customer_id: customers.sample.id
+  )
+
+  # future
+  start_at = DateTime.now + (rand(1..50)).days
+  end_at = start_at + 1.hour
+
+  Appointment.create!(
+      start_at: start_at,
+      end_at: end_at,
+      client_id: client.id,
+      customer_id: customers.sample.id
+  )
+end
+
