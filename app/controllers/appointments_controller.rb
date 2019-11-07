75dd7b92a9760085f8c29f517dc1106d9fe6525e
Rami Burpee <rami.burpee@lootcrate.com>
2019-11-07 09:49:49 -0800

    appointments#index logic

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,10 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointments = Appointment.eager_load(:client, :customer).all
+
+    render json: @appointments.as_json(only: [:start_at, :end_at], include: {
+      customer: {only: [:full_name, :created_at, :updated_at]},
+      client: {only: [:email, :created_at, :updated_at]},
+    })
   end
 end
